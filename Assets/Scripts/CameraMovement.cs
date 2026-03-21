3e61e71532672abe0b72f7d5854d811836bcda70
markalmax <markalmax782@gmail.com>
2026-03-21 19:56:31 +0200

    making animations for the cat

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class CameraMovement : MonoBehaviour
     public CinemachineCamera cinemachineCamera;
     public CinemachineOrbitalFollow orbitalFollow;
     public float lookSensitivity = 1f, zoomSensitivity = 1f;
+    public Vector2 RangeMin, RangeMax; 
     private Vector3 lookInput;
     void Start()
     {
@@ -14,6 +16,10 @@ public class CameraMovement : MonoBehaviour
         orbitalFollow = GetComponent<CinemachineOrbitalFollow>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        RangeMax.x = orbitalFollow.HorizontalAxis.Range.y;
+        RangeMax.y = orbitalFollow.VerticalAxis.Range.y;
+        RangeMin.x = orbitalFollow.HorizontalAxis.Range.x;
+        RangeMin.y = orbitalFollow.VerticalAxis.Range.x;
     }    
     void Update()
     {
@@ -21,13 +27,13 @@ public class CameraMovement : MonoBehaviour
         lookInput.y = Input.GetAxis("Mouse Y");
         orbitalFollow.HorizontalAxis.Value += lookInput.x * lookSensitivity;
         orbitalFollow.VerticalAxis.Value += lookInput.y * lookSensitivity;
-        if(orbitalFollow.VerticalAxis.Value < -90f)
+        if(orbitalFollow.VerticalAxis.Value < RangeMin.y)
         {
-            orbitalFollow.VerticalAxis.Value = -90f;
+            orbitalFollow.VerticalAxis.Value = RangeMin.y;
         }
-        if(orbitalFollow.VerticalAxis.Value > 90f)
+        if(orbitalFollow.VerticalAxis.Value > RangeMax.y)
         {
-            orbitalFollow.VerticalAxis.Value = 90f;
+            orbitalFollow.VerticalAxis.Value = RangeMax.y;
         }
         lookInput.z = Input.GetAxis("Mouse ScrollWheel");
         cinemachineCamera.Lens.FieldOfView -= lookInput.z * zoomSensitivity;
